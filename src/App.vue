5167911b3dc2e5924922e54b6d79b35c1ee3cc5a
Cethleann <54884403+musubii@users.noreply.github.com>
2021-08-28 22:39:29 -1000

    Add simple layout to App view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,72 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
+  <div id="page-container">
+    <header>
+      <h1>musictools</h1>
+      <section id="navbar">
+        <nav>
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+        </nav>
+        <div>Not signed in</div>
+      </section>
+      <hr />
+    </header>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
 <style lang="stylus">
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,700&display=swap');
+
+  html
+    /* https://uigradients.com/#eXpresso */
+    //background -webkit-linear-gradient(to bottom, #ad5389, #3c1053) /* Chrome 10-25, Safari 5.1-6 */
+    //background linear-gradient(to bottom, #ad5389, #3c1053)
+
+    background linear-gradient(135deg, #ad5389, #3c1053)
+    background-size 400% 400%
+
+    animation backgroundGradient 30s ease infinite
+
+    margin 0
+    height 100vh
+
   #app
-    font-family Avenir, Helvetica, Arial, sans-serif
+    font-family 'Open Sans', sans-serif
     -webkit-font-smoothing antialiased
     -moz-osx-font-smoothing grayscale
-    text-align center
-    color #2c3e50
-    margin-top 60px
+
+    color white
+
+  #page-container
+    max-width 600px
+    margin 40px auto
+
+  #navbar
+    display flex
+    flex-direction row
+    justify-content space-between
+
+    nav
+      display flex
+      flex-direction row
+      column-gap 1em
+
+  hr
+    margin 20px 0
+    border none
+    border-top 1px solid white
+
+  a
+    color inherit
+
+  @keyframes backgroundGradient
+    0%
+      background-position: 0 50%
+
+    50%
+      background-position: 100% 50%
+
+    100%
+      background-position: 0 50%
 </style>
