b2c65eeb4736f07d66679c015afd389188b8b21c
Cethleann <54884403+musubii@users.noreply.github.com>
2021-08-28 22:41:21 -1000

    Add octocat corner (from https://tholman.com/github-corners/)

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,21 @@
       </section>
       <hr />
     </header>
+
+    <OctocatCorner />
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  import OctocatCorner from "@/components/OctocatCorner.vue";
+
+  export default defineComponent({
+    components: { OctocatCorner },
+  });
+</script>
 
 <style lang="stylus">
   @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,700&display=swap');
